0e741599e7e1285644889ce33e11f904e8b94fc2
ptphat77 <pptphat77@gmail.com>
2023-11-11 21:54:13 +0700

    add template string %s in query

--- services.py
+++ services.py
@@ -1,14 +1,15 @@
 from config.connectDB import connectDB
 
 
-def queryExec(queryScript, isHasReturn=True):
+def queryExec(queryScript, isHasReturn=True, *datas):
     conn = None
     cur = None
     try:
         conn = connectDB
 
         cur = conn.cursor()
-        cur.execute(queryScript)
+
+        cur.execute(queryScript, (datas))
         conn.commit()
 
         if isHasReturn:
@@ -24,18 +25,18 @@ def queryExec(queryScript, isHasReturn=True):
 
 def getStartBlockNumber(networkName):
     resultQuery = queryExec(
-        "SELECT block_number FROM start_block_number WHERE network_name='{}'".format(
-            networkName
-        )
+        "SELECT block_number FROM start_block_number WHERE network_name=%s",
+        True,
+        networkName,
     )
     return resultQuery[0]
 
 
 def isExistsPreprocessedBytecode(preprocessedBytecode):
     resultQuery = queryExec(
-        "SELECT label FROM contract_dataset WHERE md5_index = md5('{}'::text)".format(
-            preprocessedBytecode
-        )
+        "SELECT label FROM contract_dataset WHERE md5_index = md5(%s)",
+        True,
+        preprocessedBytecode,
     )
     return resultQuery
 
@@ -43,37 +44,31 @@ def isExistsPreprocessedBytecode(preprocessedBytecode):
 def insertPreprocessedBytecodeToDB(preprocessedBytecode, vulnerabilities, label):
     queryExec(
         """ INSERT INTO contract_dataset (md5_index, preprocess_bytecode, vulnerabilities, label)
-            VALUES (md5('{}'::text), '{}'::text, '{}'::text, {}::bool)
-        """.format(
-            preprocessedBytecode, preprocessedBytecode, vulnerabilities, str(label)
-        ),
+            VALUES (md5(%s), %s, %s, %s)
+        """,
         False,
+        preprocessedBytecode,
+        preprocessedBytecode,
+        vulnerabilities,
+        str(label),
     )
 
 
 def updateStartBlockNumber(networkName, blockNumber):
     queryExec(
-        "UPDATE start_block_number SET block_number={} WHERE network_name='{}'".format(
-            blockNumber, networkName
-        ),
+        "UPDATE start_block_number SET block_number=%s WHERE network_name=%s",
         False,
+        blockNumber,
+        networkName,
     )
 
 
 def insertCotnractInfoToDB(address, sourceCode, bytecode, abi):
-    conn = None
-    cur = None
-    try:
-        conn = connectDB
-
-        cur = conn.cursor()
-        queryScript = """INSERT INTO contract_info (address, source_code, bytecode, abi) VALUES (%s, %s, %s, %s) ON CONFLICT DO NOTHING"""
-        data = (address, sourceCode, bytecode, abi)
-        cur.execute(queryScript, data)
-        conn.commit()
-
-    except Exception as error:
-        print(">>> Database error: ", error)
-        print(">>> Query script: ", " ".join((queryScript.split())[:2]), "...")
-        # print(">>> Query script: ", queryScript)
-        exit()
+    queryExec(
+        "INSERT INTO contract_info (address, source_code, bytecode, abi) VALUES (%s, %s, %s, %s) ON CONFLICT DO NOTHING",
+        False,
+        address,
+        sourceCode,
+        bytecode,
+        abi,
+    )
