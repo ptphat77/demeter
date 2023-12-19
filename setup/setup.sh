7675074e0792196d7986cfd16b5ac825823e53de
Ter-Rung <20522075@gm.uit.edu.vn>
2023-12-19 11:39:13 +0700

    add setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -17,3 +17,29 @@ add-apt-repository ppa:ethereum/ethereum -y
 apt install solc -y
 apt install libssl-dev python3-dev python3-pip -y
 pip3 install mythril
+
+# Install Maian tool
+# -> require go-ethereum
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install ethereum
+# -> require solidity compiler
+sudo add-apt-repository ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install solc
+
+# -> require z3 
+git clone https://github.com/Z3Prover/z3.git
+python scripts/mk_make.py
+cd build
+make
+sudo make install
+# -> require web3
+pip install web3
+
+git clone https://github.com/ivicanikolicsg/MAIAN.git
+#cd MAIAN/tool
+#Suicidal contracts use -c 0
+#Prodigal contracts use -c 1
+#Greedy contracts use -c 2
+#python maian.py -b <bytecode_file> -c <0,1,2>
